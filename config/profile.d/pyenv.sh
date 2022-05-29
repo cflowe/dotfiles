530fff2daa5153ae8f2e50d50f81ef797c07abe4
Cleveland Flowe <flowe.c@gmail.com>
2022-05-29 13:49:06 -0400

    Added an ssh-config.d dir

--- config/profile.d/pyenv.sh
+++ config/profile.d/pyenv.sh
@@ -1,7 +1,19 @@
-export PYENV_ROOT="${HOME}/local/pyenv"
-export PATH="$PYENV_ROOT/bin:$PATH"
+if [ -x "${PYENV_ROOT:-${HOME}/local/pyenv}/bin/pyenv" ]; then
+  export PYENV_ROOT="${PYENV_ROOT:-${HOME}/local/pyenv}"
 
-if command -v pyenv &> /dev/null; then
-  eval "$(pyenv init -)"
-  eval "$(pyenv virtualenv-init -)"
+  case "$PATH" in
+  *${PYENV_ROOT}/bin*) ;;
+  *) export PATH="${PYENV_ROOT}/bin:${PATH}";;
+  esac
+
+  if ! command -v pyenv &> /dev/null; then
+    echo "pyenv is not in PATH '$PATH'" >&2
+  else
+    eval "$(pyenv init --path)"
+    eval "$(pyenv init -)"
+
+    if [ -d "${PYENV_ROOT}/plugins/pyenv-virtualenv/" ]; then
+      eval "$(pyenv virtualenv-init -)"
+    fi
+  fi
 fi
